232175d4b4de556a7bb433a9ee0ec80fe25ce25f
amihaliov <amihaliov@noreply.com>
2022-06-24 17:24:37 +0300

    Change Ingredients ID from Long to UUID. Closes #7

--- src/main/java/com/amihaliov/spring_jpa_demo/model/Ingredient.java
+++ src/main/java/com/amihaliov/spring_jpa_demo/model/Ingredient.java
@@ -1,13 +1,29 @@
 package com.amihaliov.spring_jpa_demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "ingredients")
-public class Ingredient extends BaseEntity {
+public class Ingredient /*extends BaseEntity*/ {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
 
     private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
